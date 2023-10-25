11f932e0b1d48206a82096cd56c852b6015439ef
kryvokhyzha <kriwohizha@gmail.com>
2023-10-25 18:11:09 +0300

    minore refactoring

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 
-from backend.src.connector import RedisConnector
+from backend.src.redis_connector import RedisConnector
 from backend.src.router import router as main_router
 
 
 app = FastAPI()
-_ = RedisConnector().connect(host="redis", port=6379)
+_ = (
+    RedisConnector()
+    .update_connection(host="redis", port=6379)
+    .update_queue("high")
+    .update_queue("default")
+    .update_queue("low")
+)
 
 app.include_router(main_router, prefix="", tags=["Main router"])
--- backend/src/redis_connector.py
+++ backend/src/redis_connector.py
@@ -7,16 +7,24 @@ from backend.src.helper.utils import SingletonMeta
 class RedisConnector(metaclass=SingletonMeta):
     """Singleton class for Redis connection."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs) -> None:
         """Initialize Redis connection."""
-        self.connection = None
-        self.high_queue = None
-        self.default_queue = None
-        self.low_queue = None
-        self.queues = tuple()
+        self.connection = Redis(*args, **kwargs)
+        self.queues = dict()
 
-    def connect(self, *args, **kwargs) -> "RedisConnector":
-        """Connect to Redis and initialize queues.
+    def update_queue(self, queue_name: str) -> "RedisConnector":
+        """Add new queue to Redis connection or update existing one.
+
+        Parameters
+        ----------
+        queue_name : str
+            Name of new queue.
+        """
+        self.queues.update({queue_name: Queue(queue_name, connection=self.connection)})
+        return self
+
+    def update_connection(self, *args, **kwargs) -> "RedisConnector":
+        """Update connection to Redis.
 
         Parameters
         ----------
@@ -31,11 +39,4 @@ class RedisConnector(metaclass=SingletonMeta):
             Instance of RedisConnector.
         """
         self.connection = Redis(*args, **kwargs)
-
-        self.high_queue = Queue("high", connection=self.connection)
-        self.default_queue = Queue("default", connection=self.connection)
-        self.low_queue = Queue("low", connection=self.connection)
-
-        self.queues = (self.high_queue, self.default_queue, self.low_queue)
-
         return self
